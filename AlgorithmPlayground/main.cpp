1997a4210d87d37602ee00820d2c55344cc6657f
hayabusabusa <hayabusabusaSY@gmail.com>
2023-02-11 13:05:10 +0900

    chore: プロジェクトを追加

---
+++ AlgorithmPlayground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AlgorithmPlayground
+//
+//  Created by Shunya Yamada on 2023/02/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
